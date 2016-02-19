7c4625bb5ccb97d867cdd97cee7812ff0fce3411
guadux <guadalupe.alonso@gmail.com>
2016-02-19 11:26:48 -0300

    Menu :)

--- app/src/main/java/com/invixion/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/invixion/temperatureconverter/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.app.Activity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (EditText) findViewById(R.id.inputValue);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     public void clickTemp(View view) {
