ab476c43b8c6adfd4ab4842357b0e1474438a0c9
guadux <guadalupe.alonso@gmail.com>
2016-02-19 14:50:36 -0300

    Menu con cartel y con boton hide

--- app/src/main/java/com/invixion/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/invixion/temperatureconverter/MainActivity.java
@@ -1,31 +1,71 @@
 package com.invixion.temperatureconverter;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText text;
+    private TextView info;
+//    private ActionBar mActionbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (EditText) findViewById(R.id.inputValue);
+        info =(TextView)findViewById(R.id.info);
+//        mActionbar = getActionBar();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.add:
+                //metodoAdd()
+                Toast.makeText(this, "Ha presionado add",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.search:
+                //metodoSearch()
+
+                return true;
+            case R.id.edit:
+                //metodoEdit()
+
+                return true;
+            case R.id.delete:
+                //metodoDelete()
+                //escondemos la actionbar
+                ActionBar actionBar = getSupportActionBar();
+                actionBar.hide();
+                return true;
+            case R.id.action_settings:
+                //metodoSettings()
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void clickTemp(View view) {
